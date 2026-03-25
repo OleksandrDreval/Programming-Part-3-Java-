cf051fd5a35e39b85974fdbb28b771133c472440
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-03-25 18:18:55 +0200

    Add tests for formatting the display of a book in the BookDisplayFormatter class

---
+++ src/test/java/ua/nure/ice/bookcatalog/ui/BookDisplayFormatterTest.java
@@ -0,0 +1,23 @@
+package ua.nure.ice.bookcatalog.ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import ua.nure.ice.bookcatalog.model.Book;
+import ua.nure.ice.bookcatalog.model.BookGenre;
+
+class BookDisplayFormatterTest {
+
+  @Test
+  void formatShouldContainAllBookFields() {
+    Book book = new Book(7L, "Dune", "Frank Herbert", 1965, BookGenre.SCIENCE_FICTION);
+
+    String formattedBook = BookDisplayFormatter.format(book);
+
+    assertTrue(formattedBook.contains("ID: 7"));
+    assertTrue(formattedBook.contains("Title: Dune"));
+    assertTrue(formattedBook.contains("Author: Frank Herbert"));
+    assertTrue(formattedBook.contains("Year: 1965"));
+    assertTrue(formattedBook.contains("Genre: SCIENCE_FICTION"));
+  }
+}
